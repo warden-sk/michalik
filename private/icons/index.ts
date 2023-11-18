91d496d93a7f3d559fcf3eeeb7e2a30e5e8f1960
Marek Kobida <marek.kobida@gmail.com>
2023-11-18 23:33:24 +0800

    ikony

---
+++ private/icons/index.ts
@@ -0,0 +1,21 @@
+/*
+ * Copyright 2023 Marek Kobida
+ */
+
+import CalendarIcon from './CalendarIcon';
+import LeftArrowIcon from './LeftArrowIcon';
+import LinkIcon from './LinkIcon';
+import MailIcon from './MailIcon';
+import MapIcon from './MapIcon';
+import PhoneIcon from './PhoneIcon';
+
+const icons = {
+  CalendarIcon,
+  LeftArrowIcon,
+  LinkIcon,
+  MailIcon,
+  MapIcon,
+  PhoneIcon,
+};
+
+export default icons;
