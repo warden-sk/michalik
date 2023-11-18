cd121d59c8a91fadc0436180c9c5a931639fd146
Marek Kobida <marek.kobida@gmail.com>
2023-11-18 23:12:28 +0800

    updated

--- private/ProjectsProject.tsx
+++ private/ProjectsProject.tsx
@@ -4,23 +4,15 @@
 
 import React from 'react';
 import type { Project } from './helpers/types';
+import Photo from './Photo';
 import Text from './Text';
 
 function ProjectsProject({ description, i, id, labels, name, photoUrl }: Project & { i: number }) {
   return (
     <a className="ProjectsProject" href={`#/project/${id}`} spaceY="2">
       <Text size={6}>{(i + 1).toString().padStart(3, '0')}</Text>
-      <img borderRadius="3" className="Photo ProjectsProjectPhoto" display="block" src={photoUrl} width="100" />
-      {labels.length > 0 && (
-        <div display="flex" spaceX="2">
-          {labels.map(label => (
-            <Text className="Label" key={label} pX="3" pY="1" size={2}>
-              {label}
-            </Text>
-          ))}
-        </div>
-      )}
-      <div>
+      <Photo labels={labels} url={photoUrl} />
+      <div pX="2">
         <Text fontWeight="500">{name}</Text>
         <Text lineHeight="2" opacity="50">
           {description[0]}
