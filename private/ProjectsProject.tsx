7665dfc5f867a96dd570624b1c79660aee760025
Marek Kobida <marek.kobida@gmail.com>
2023-11-21 15:24:03 +0800

    farbičky

--- private/ProjectsProject.tsx
+++ private/ProjectsProject.tsx
@@ -11,11 +11,11 @@ type P = Project & {
   i: number;
 };
 
-function ProjectsProject({ i, id, labels, name, photoUrl }: P) {
+function ProjectsProject({ i, id, labels, name, photoUrl, type }: P) {
   return (
     <a className="ProjectsProject" href={`#/project/${id}`} spaceY="2">
       <Text size={6}>{(i + 1).toString().padStart(3, '0')}</Text>
-      <Photo labels={labels} url={`./projects/${id}/${photoUrl}`} />
+      <Photo labels={labels} type={type} url={`./projects/${id}/${photoUrl}`} />
       <div pX="2">
         <Text fontWeight="500" lineHeight="2">
           {name}
--- private/helpers/types.ts
+++ private/helpers/types.ts
@@ -9,6 +9,7 @@ type Project = {
   name: string;
   photoUrl: string;
   rows: string[][];
+  type: 'b' | 'w';
   when: number;
   where: string;
   who: string;
